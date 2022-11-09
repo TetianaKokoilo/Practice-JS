2a4d4d6c26881639904a9e9ff3c1b7e518ed63f8
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-09 17:54:10 +0200

    add tasks lessons2

--- js/lesson2.js
+++ js/lesson2.js
@@ -212,3 +212,44 @@
     
 // }
 // console.log('Total =', total);
+
+
+
+
+
+// Задача 9. Работаем с коллекцией карточек Trello
+//  - Метод Splice()
+//  - Удалить
+//  - Добавить
+//  - Обновить
+
+// const cards = ['Карточка-1', 'Карточка-2', 'Карточка-3', 'Карточка-4', 'Карточка-5'];
+// console.table(cards);
+
+// Удаление (по индексу), метод indexOf()
+
+// const cardToRemove = 'Карточка-3';
+// const index = cards.indexOf(cardToRemove);
+// console.log(index);
+
+// cards.splice(index, 1);
+// console.table(cards);
+
+// Добавление по индексу
+
+// const cardToInsert = 'Карточка-6';
+// const index = 3;
+
+// cards.splice(index, 0, cardToInsert);
+
+
+// console.table(cards);
+
+// Обновление за индексом
+
+// const cardToUpdate = 'Карточка-4';
+// const index = cards.indexOf(cardToUpdate);
+// console.log(index);
+
+// cards.splice(index, 1, cardToUpdate);
+// console.table(cards);
