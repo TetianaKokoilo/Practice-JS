3f7bc92ecc93fcbf01e3be5016fd75bbe060499c
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-09 17:15:31 +0200

    add task 8

--- js/lesson2.js
+++ js/lesson2.js
@@ -193,3 +193,22 @@
 // // 2 вариант
 
 // console.log(title.toLowerCase().split(' ').join('-'));
+
+
+
+
+
+// Задача 8. Написать скрипт который считает сумму элементов двух масивов
+
+// const array1 = [5, 10, 15, 20];
+// const array2 = [10, 20, 30];
+// let total = 0;
+
+// const merge = array1.concat(array2);
+// console.log(merge);
+
+// for (const value of merge) {
+//     total += value;
+    
+// }
+// console.log('Total =', total);
