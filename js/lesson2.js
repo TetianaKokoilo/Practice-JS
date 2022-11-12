d18f8c7a471fee2d6b71deeb2133c45490b08d70
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-12 13:22:22 +0200

    add lesson2 function

--- js/lesson2.js
+++ js/lesson2.js
@@ -478,6 +478,60 @@
 //  - Array.from()
 //  - операция ... rest
 
-const add = function () {
+// 1 вариант
+// const add = function () {
+//     console.log(arguments);
+
+//     const args = Array.from(arguments);
+//     console.log(args);
     
+//     let total = 0;
+//     for (const arg of arguments) {
+//         total += arg;
+        
+//     }
+//     return total;
+// }
+
+// console.log(add(1, 2, 3));
+// console.log(add(1, 3, 4, 5, 6));
+
+// 2 вариант
+
+// const add = function (...args) {
+//     console.log(args);
+//     let total = 0;
+//     for (const arg of args) {
+//         total += arg;
+        
+//     }
+//     return total;
+
+// }
+
+
+// console.log(add(1, 2, 3));
+// console.log(add(1, 3, 4, 5, 6));
+
+// Напиши функцию filterNumbers(array [, number1, ...]) которая:
+//  - первым аргументом принимает массив чисел
+//  - после первого аргумента может быть произвольное количество других аргументов, которые будут числами
+//  - Функция должна вернуть новый массив , в котором будут только те аргументы, начиная со второго,
+// для которых есть аналог в оригинальном массиве
+
+const filterNumbers = function (array, ...args) {
+    console.log('array = ', array);
+    console.log('args = ', args);
+    const newElement = [];
+
+    for (const element of array) {
+        if (args.includes(element)) {
+            newElement.push(element);
+        }
+    }
+    return newElement;
 }
+
+console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8));
+console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15));
+console.log(filterNumbers([100, 200, 300, 400, 500], 7, 12, 200, 64));
