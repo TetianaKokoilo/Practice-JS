ebe4e14ecac44aaca99fd0f749dd5443749b871c
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-25 14:43:50 +0200

    Update lesson3-1.js

--- js/lesson3-1.js
+++ js/lesson3-1.js
@@ -538,7 +538,7 @@
 //   for (const product of products) {
 //     if (productName === product.name) {
 //       return product.price;
-//     } 
+//     }
        
     
     
@@ -552,3 +552,33 @@
 // }
 // console.log(getProductPrice("Radar"));
 // console.log(getProductPrice("fff"));
+
+
+
+
+
+
+
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+// function calculateTotalPrice(productName) {
+//   // Change code below this line
+//   let result;
+//   for (const product of products) {
+//     if (productName === product.name) {
+//       result = product.price * product.quantity;
+//       return result
+//     }
+    
+//   }
+
+//   return 0;
+//   // Change code above this line
+// }
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Scavxxvner"));
