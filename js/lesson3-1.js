4c5d1b7eb9efae2f006a50e74e827d0812d0c214
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-25 16:03:41 +0200

    Update lesson3-1.js

--- js/lesson3-1.js
+++ js/lesson3-1.js
@@ -609,16 +609,39 @@
 
 
 // Change code below this line
-function add(...args) {
-  console.log(args);
-  let sum = 0;
-  for (const arg of args) {
-    sum += arg;
+// function add(...args) {
+//   console.log(args);
+//   let sum = 0;
+//   for (const arg of args) {
+//     sum += arg;
     
     
-  }
-  return sum;
-  // Change code above this line
-}
+//   }
+//   return sum;
+//   // Change code above this line
+// }
 
-console.log(add(15, 27));
+// console.log(add(15, 27));
+
+
+
+
+
+
+// Change code below this line
+// function addOverNum(a, ...args) {
+//   let total = 0;
+
+//   for (const arg of args) {
+//     if (arg > a) {
+//       total += arg;
+//     }
+    
+//   }
+
+//   return total;
+//   // Change code above this line
+// }
+
+// console.log(addOverNum(50, 15, 27));
+// console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
