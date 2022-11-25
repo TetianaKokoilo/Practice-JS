9edc4eee062756da1f4d6b2aa874403945475d1e
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-25 12:34:12 +0200

    Update lesson3-1.js

--- js/lesson3-1.js
+++ js/lesson3-1.js
@@ -476,20 +476,79 @@
 // }));
 
 
-function countTotalSalary(salaries) {
-  let totalSalary = 0;
-  // Change code below this line
-  const values = Object.values(salaries);
-  console.log(values);
-  for (const value of values) {
-    totalSalary += value;
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   const values = Object.values(salaries);
+//   console.log(values);
+//   for (const value of values) {
+//     totalSalary += value;
     
-  }
-  // Change code above this line
-  return totalSalary;
-}
-console.log(countTotalSalary({
-  mango: 100,
-  poly: 150,
-  alfred: 80
-}));
+//   }
+//   // Change code above this line
+//   return totalSalary;
+// }
+// console.log(countTotalSalary({
+//   mango: 100,
+//   poly: 150,
+//   alfred: 80
+// }));
+
+
+
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+// }
+// const rgbColors = [];
+// for (const color of colors) {
+//   rgbColors.push(color.rgb);
+  
+// }
+// // Change code below this line
+
+
+
+// console.log(hexColors);
+// console.log(rgbColors);
+
+
+
+
+
+
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (productName === product.name) {
+//       return product.price;
+//     } 
+       
+    
+    
+
+//   }
+  
+
+// return null;
+
+//   // Change code above this line
+// }
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("fff"));
