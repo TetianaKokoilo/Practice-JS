152e3a95532207dd1004f96a6c8bbaa71506b7a6
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-28 19:19:56 +0200

    Update lesson4.js

--- js/lesson4.js
+++ js/lesson4.js
@@ -583,30 +583,151 @@
 
 
 // 5
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: true },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: false },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+// ];
+
+// /*
+//  * Array.prototype.every()
+//  * - Поэлементо перебирает оригинальный массив
+//  * - Возвращает true если все элементы массива удовлетворяют условию
+//  */
+
+// const isAllOnline = players.every(player => player.online);
+// console.log('isAllOnline: ', isAllOnline);
+
+// /*
+//  * Array.prototype.some()
+//  * - Поэлементо перебирает оригинальный массив
+//  * - Возвращает true если хотя бы один элемент массива удовлетворяет условию
+//  */
+// const isAnyOnline = players.some(player => player.online);
+// console.log('isAnyOnline: ', isAnyOnline);
+
+// const anyHardcorePlayers = players.some(player => player.timePlayed > 400);
+// console.log('anyHardcorePlayers: ', anyHardcorePlayers);
+
+
+
+
+
+
+/* 6
+ * Array.prototype.reduce()
+ * - Поэлементо перебирает оригинальный массив
+ * - Возвращает что угодно 🤯
+ * - Заменяет всё на свете, но использовать нужно с умом
+ */
+
+const numbers = [5, 10, 15, 20, 25];
+
+const total = numbers.reduce((acc, number) => acc + number, 0);
+// console.log(total);
+
+// accumulator = 0 -> number = 5 -> return 0 + 5
+// accumulator = 5 -> number = 10 -> return 5 + 10
+// accumulator = 15 -> number = 15 -> return 15 + 15
+// accumulator = 30 -> number = 20 -> return 30 + 20
+// accumulator = 50 -> number = 25 -> return 50 + 25
+
+/*
+ * Считаем общую зарплату
+ */
+
+const salary = {
+  mango: 100,
+  poly: 50,
+  ajax: 150,
+};
+
+const totalSalary = Object.values(salary).reduce(
+  (total, value) => total + value,
+  0,
+);
+// console.log(totalSalary);
+
+/*
+ * Считаем общее количество часов
+ */
+
 const players = [
-  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: true },
-  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: false },
-  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
-  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
-  { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+  { id: 'player-1', name: 'Mango', timePlayed: 310, online: false },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, online: true },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 230, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, online: false },
+  { id: 'player-5', name: 'Chelsey', timePlayed: 80, online: true },
 ];
 
-/*
- * Array.prototype.every()
- * - Поэлементо перебирает оригинальный массив
- * - Возвращает true если все элементы массива удовлетворяют условию
- */
+const totalTimePlayed = players.reduce(
+  (totalTime, player) => totalTime + player.timePlayed,
+  0,
+);
 
-const isAllOnline = players.every(player => player.online);
-console.log('isAllOnline: ', isAllOnline);
+// console.log(totalTimePlayed);
 
 /*
- * Array.prototype.some()
- * - Поэлементо перебирает оригинальный массив
- * - Возвращает true если хотя бы один элемент массива удовлетворяет условию
+ * Считаем общую сумму товаров корзины
  */
-const isAnyOnline = players.some(player => player.online);
-console.log('isAnyOnline: ', isAnyOnline);
+const cart = [
+  { label: 'Apples', price: 100, quantity: 2 },
+  { label: 'Bananas', price: 120, quantity: 3 },
+  { label: 'Lemons', price: 70, quantity: 4 },
+];
 
-const anyHardcorePlayers = players.some(player => player.timePlayed > 400);
-console.log('anyHardcorePlayers: ', anyHardcorePlayers);
+const totalAmount = cart.reduce(
+  (total, { price, quantity }) => total + price * quantity,
+  0,
+);
+
+// console.log(totalAmount);
+
+/*
+ * Собираем все теги из твитов
+ */
+const tweets = [
+  { id: '000', likes: 5, tags: ['js', 'nodejs'] },
+  { id: '001', likes: 2, tags: ['html', 'css'] },
+  { id: '002', likes: 17, tags: ['html', 'js', 'nodejs'] },
+  { id: '003', likes: 8, tags: ['css', 'react'] },
+  { id: '004', likes: 0, tags: ['js', 'nodejs', 'react'] },
+];
+
+const allTags = tweets.reduce((acc, tweet) => [...acc, ...tweet.tags], []);
+console.log(allTags);
+
+// acc = [], tweet.tags = ['js', 'nodejs'] return [...[], ...['js', 'nodejs']]
+// acc = ['js', 'nodejs'] tweet.tags ['html', 'css']
+// return  [...['js', 'nodejs'], ...['html', 'css']]
+//  ['js', 'nodejs', 'html', 'css']
+
+/*
+ * Ведём статистику тегов
+ */
+// const tagsStats = allTags.reduce((acc, tag) => {
+//   console.log(acc);
+
+//   if (acc[tag]) {
+//     acc[tag] += 1;
+
+//     return acc;
+//   }
+
+//   acc[tag] = 1;
+
+//   return acc;
+// }, {});
+
+const tagsStats = allTags.reduce((acc, tag) => {
+  return {
+    ...acc,
+    [tag]: acc[tag] ? acc[tag] + 1 : 1,
+  };
+}, {});
+// console.log(tagsStats);
+
+// если свойство с ключом tag есть. увеличить его значение на 1
+// если свойствоства нет с таким ключом что в tag, сделать и записать 1
