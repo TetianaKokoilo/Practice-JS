84b34b7aff3f6ef4eab7adecd5694e226531dce3
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-28 14:01:41 +0200

    add task 2

--- js/lesson4-1.js
+++ js/lesson4-1.js
@@ -12,42 +12,100 @@
 
 
 // your code here
-const product = {
-  name: "chocolate",
-  price: 34,
-  quantity: 5
-}
+// const product = {
+//   name: "chocolate",
+//   price: 34,
+//   quantity: 5
+// }
 
-const createProduct = (obj, callback) => {
-  // створили новий обʼєкт товару, оператором спред забралм всі властивости із параметра obj
-  const product = {
-    ...obj,
-    id: 1
-  };
+// const createProduct = (obj, callback) => {
+//   // створили новий обʼєкт товару, оператором спред забралм всі властивости із параметра obj
+//   const product = {
+//     ...obj,
+//     id: 1
+//   };
 
-  // викликали функцію callback яку передають в параметрах. Передали їй новий обʼєкт як аргумент
-  // Повернули результат виконання колбека
-  return callback(product)
+//   // викликали функцію callback яку передають в параметрах. Передали їй новий обʼєкт як аргумент
+//   // Повернули результат виконання колбека
+//   return callback(product)
+// };
+
+// // Створили просто окрему функцію, яка приймає один параметр(будь-що) і виводить його в консоль. 
+// const logger = (val) => {
+//   console.log(val)
+// }
+
+// // Створили ще одну окрему функцію якак приймає параметром обʼєкт(товар) і рахує вартість.
+// const calculateTotalPrice = ({price, quantity}) => {
+//   const total = price * quantity;
+//   return total
+// }
+
+// /**
+//  * коротша форма запису:
+//  * const logger = val => console.log(val)
+//  * const calculateTotalPrice = ({price, quantity}) => price * quantity
+//  */
+
+// //викликали createProduct. Передали два аргументи: обʼєкт товара і будь-яку функцію колбек
+// createProduct(product, logger); // колбеком буде функція looger
+// const totalPrice = createProduct(product, calculateTotalPrice); // колбеком буде функція calculateTotalPrice
+// console.log(totalPrice)
+
+
+
+
+
+
+
+// Example 2 - Коллбек функції
+// Додайте об'єкт account методи withdraw(amount, onSuccess, onError) та deposit(amount, onSuccess, onError), 
+// де перший параметр це сума операції, а другий та третій - коллбеки.
+
+// Метод withdraw викликає onError якщо amount більше TRANSACTION_LIMIT або this.balance, і onSuccess в іншому випадку.
+
+// Метод deposit викликає onError якщо amount більше TRANSACTION_LIMIT або менше або дорівнює нулю, 
+// і onSuccess в іншому випадку.
+
+
+const TRANSACTION_LIMIT = 1000;
+
+const account = {
+  username: 'Jacob',
+  balance: 40000,
+  
+  withdraw (amount, onSuccess, onError) {
+    if(amount > this.balance) {
+      return onError(amount, 'Недостатньо балансу')
+    }
+    
+    if(amount > TRANSACTION_LIMIT) {
+      return onError(amount, 'перевищенно ліміт операцій')
+    }
+
+    onSuccess(amount);
+  },
+
+  deposit (amount, onSuccess, onError) {
+    if (amount > TRANSACTION_LIMIT || amount <= 0) {
+      return onError(amount)
+    }
+
+    onSuccess(amount);
+  },
 };
 
-// Створили просто окрему функцію, яка приймає один параметр(будь-що) і виводить його в консоль. 
-const logger = (val) => {
-  console.log(val)
+const handleSuccess = (amount) => {
+  console.log(`${amount} успішно опрацьовано!`)
 }
 
-// Створили ще одну окрему функцію якак приймає параметром обʼєкт(товар) і рахує вартість.
-const calculateTotalPrice = ({price, quantity}) => {
-  const total = price * quantity;
-  return total
+const handleError = (amount, message = 'Невідомо') => {
+  console.log(`${amount} Не опрацьовано! По причині: ${message}`)
 }
-
-/**
- * коротша форма запису:
- * const logger = val => console.log(val)
- * const calculateTotalPrice = ({price, quantity}) => price * quantity
- */
-
-//викликали createProduct. Передали два аргументи: обʼєкт товара і будь-яку функцію колбек
-createProduct(product, logger); // колбеком буде функція looger
-const totalPrice = createProduct(product, calculateTotalPrice); // колбеком буде функція calculateTotalPrice
-console.log(totalPrice)
+account.withdraw(2000, handleSuccess, handleError);
+account.withdraw(600, handleSuccess, handleError);
+account.withdraw(300, handleSuccess, handleError);
+account.deposit(1700, handleSuccess, handleError);
+account.deposit(0, handleSuccess, handleError);
+account.deposit(-600, handleSuccess, handleError);
+account.deposit(600, handleSuccess, handleError);
