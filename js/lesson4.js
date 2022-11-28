1f956f2b06ea226d028f9ea7d014d362154a08c3
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-28 18:46:01 +0200

    add task 4

--- js/lesson4.js
+++ js/lesson4.js
@@ -494,36 +494,84 @@
  *    - если коллбек вернул false элемент НЕ добавляется в возвращаемый массив
  */
 
-const numbers = [5, 10, 15, 20, 25];
+// const numbers = [5, 10, 15, 20, 25];
 
-const filteredNumbers = numbers.filter(number => number < 10 || number > 20);
-// console.log(filteredNumbers);
+// const filteredNumbers = numbers.filter(number => number < 10 || number > 20);
+// // console.log(filteredNumbers);
 
-const players = [
-  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
-  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
-  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
-  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
-  { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
-];
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+// ];
 
-/*
- * Получаем массив всех онлайн игроков
+// /*
+//  * Получаем массив всех онлайн игроков
+//  */
+
+// const onlinePlayers = players.filter(({ online }) => online);
+// console.table(onlinePlayers);
+
+// /*
+//  * Получаем массив всех оффлайн игроков
+//  */
+
+// const offlinePlayers = players.filter(player => !player.online);
+// console.table(offlinePlayers);
+
+// /*
+//  * Получаем список хардкорных игроков с временем больше 250
+//  */
+
+// const hardcorePlayers = players.filter(player => player.timePlayed > 250);
+// console.table(hardcorePlayers);
+
+
+
+
+
+
+
+
+
+/* 4
+ * Array.prototype.find()
+ * - Поэлементо перебирает оригинальный массив
+ * - Возвращает первый элемент удовлетворяющий условию или undefined
  */
 
-const onlinePlayers = players.filter(({ online }) => online);
-console.table(onlinePlayers);
+// const numbers = [5, 10, 15, 20, 25];
 
-/*
- * Получаем массив всех оффлайн игроков
- */
+// const number = numbers.find(number => number === 10);
+// // console.log(number);
 
-const offlinePlayers = players.filter(player => !player.online);
-console.table(offlinePlayers);
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+// ];
 
-/*
- * Получаем список хардкорных игроков с временем больше 250
- */
+// /*
+//  * Ищем игрока по id
+//  */
+// const playerIdToFind = 'player-3';
+// const playerWithId = players.find(({ id }) => id === playerIdToFind);
+// // console.log(playerWithId);
 
-const hardcorePlayers = players.filter(player => player.timePlayed > 250);
-console.table(hardcorePlayers);
+// const finPlayerById = (allPlayer, playerId) => {
+//   return allPlayer.find(({ id }) => id === playerId);
+// };
+
+// console.log(finPlayerById(players, 'player-1'));
+// console.log(finPlayerById(players, 'player-4'));
+
+// /*
+//  * Ищем игрока по имени
+//  */
+// const playerNameToFind = 'Poly';
+// const playerWithName = players.find(player => player.name === playerNameToFind);
+// // console.log(playerWithName);
