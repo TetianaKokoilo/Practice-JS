68fe786b6642349ffa8c75290b2ac2f5e2c0243e
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-28 15:58:58 +0200

    Update lesson4-1.js

--- js/lesson4-1.js
+++ js/lesson4-1.js
@@ -251,16 +251,16 @@
 // Example 9 - Метод forEach
 // Виконайте рефакторинг коду за допомогою методу forEach та стрілочні функції.
 
-function calсulateAverage(...args) {
-  let total = 0;
-//   for (let i = 0; i < args.length; i++) {
-//     total += args[i];
-//   }
-    args.forEach(arg => total += arg);
+// function calсulateAverage(...args) {
+//   let total = 0;
+// //   for (let i = 0; i < args.length; i++) {
+// //     total += args[i];
+// //   }
+//     args.forEach(arg => total += arg);
 
-  return total / args.length;
-}
+//   return total / args.length;
+// }
 
-console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
-console.log(calсulateAverage(14, 8, 2)); // 8
-console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
+// console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
+// console.log(calсulateAverage(14, 8, 2)); // 8
+// console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
