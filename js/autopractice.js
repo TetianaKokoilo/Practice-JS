a8f8d9263b9bce92124209740f4109ab64fbaa92
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-11-29 14:20:04 +0200

    add tasks 1-7

---
+++ js/autopractice.js
@@ -0,0 +1,147 @@
+// 1
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// // Change code below this line
+
+// const result =makePizza();
+// const pointer =  makePizza;
+
+// console.log(result);
+// console.log(pointer);
+
+// 2
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//     return callback(pizzaName);
+
+// }
+
+// console.log(makeMessage("Royal Grand", makePizza));
+// console.log(makeMessage("Ultracheese", deliverPizza));
+
+// 3
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}.`);
+// });
+// // Change code below this line
+
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+//     console.log(`Eating pizza ${pizzaName}.`);
+// });
+
+
+
+// 4
+// const pizzaPalace = {
+//   pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+//   order(pizzaName, makePizza, onOrderError) {
+//     if (this.pizzas.includes(pizzaName)) {
+//       return makePizza(pizzaName);
+//     }
+
+//     return onOrderError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+//   },
+// };
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// console.log(pizzaPalace.order('Smoked', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Four meats', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Big Mike', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Vienna', makePizza, onOrderError));
+
+
+
+// 5
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Change code below this line
+
+// //   for (let i = 0; i < orderedItems.length; i += 1) {
+// //     totalPrice += orderedItems[i];
+// //     }
+//     orderedItems.forEach(orderedItem => totalPrice += orderedItem);
+
+//   // Change code above this line
+//   return totalPrice;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+
+
+
+// 6
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   // Change code below this line
+
+// //   for (let i = 0; i < numbers.length; i += 1) {
+// //     if (numbers[i] > value) {
+// //       filteredNumbers.push(numbers[i]);
+// //     }
+// //   }
+//     numbers.forEach(number => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//         }
+//         return filteredNumbers
+//     });
+
+//   // Change code above this line
+//   return filteredNumbers;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+
+
+
+// 7
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
+
+// //   for (let i = 0; i < firstArray.length; i += 1) {
+// //     if (secondArray.includes(firstArray[i])) {
+// //       commonElements.push(firstArray[i]);
+// //     }
+// //   }
+//     firstArray.forEach(firstArr => {
+//         if (secondArray.includes(firstArr)) {
+//             commonElements.push(firstArr);
+//             return commonElements;
+//         }
+//     });
+
+//   return commonElements;
+//   // Change code above this line
+// }
+
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
