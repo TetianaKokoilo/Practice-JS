efc2d82c5d68f351082878073a9a6a54b410134a
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-01 17:06:16 +0200

    add lesson5

--- js/autopractice.js
+++ js/autopractice.js
@@ -489,22 +489,3 @@
 //   .sort((a, b) => a.localeCompare(b));
 
 // console.log(names);
-class User {
-  static #takenEmails = [];
-
-  static isEmailTaken(email) {
-    return User.#takenEmails.includes(email);
-  }
-
-  #email;
-
-  constructor({ email }) {
-    this.#email = email;
-    User.#takenEmails.push(email);
-  }
-}
-
-const mango = new User({ email: "mango@mail.com" });
-
-console.log(User.isEmailTaken("poly@mail.com"));
-console.log(User.isEmailTaken("mango@mail.com"));
