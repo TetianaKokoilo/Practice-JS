874c1bddd89c7bf3efe417d7bc862deb90aaf21a
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-02 17:16:34 +0200

    counter

--- js/lesson5.js
+++ js/lesson5.js
@@ -296,3 +296,44 @@
 // updateCounter(5, counter.decrement.bind(counter));
 
 // console.log(counter);
+
+
+
+
+
+
+
+const counter = {
+    value: 0,
+    increment() {
+        console.log('increment -> this', this);
+        this.value += 1;
+    },
+    decrement() {
+        console.log('decrement -> this', this);
+        this.value -= 1;
+    },
+};
+
+
+const decrementBtn = document.querySelector('.js-decrement');
+const incrementBtn = document.querySelector('.js-increment');
+const valueEl = document.querySelector('.js-value');
+
+decrementBtn.addEventListener('click', function () {
+    console.log('Кликнули на декремент');
+
+    counter.decrement();
+    console.log(counter);
+    valueEl.textContent = counter.value;
+});
+
+incrementBtn.addEventListener('click', function () {
+    console.log('Кликнули на инкремент');
+
+    counter.increment();
+    console.log(counter);
+    valueEl.textContent = counter.value;
+});
+
+console.log(window);
