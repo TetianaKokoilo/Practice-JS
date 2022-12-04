3988bfd47b2cf3c7307eea74744f029704298f81
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2022-12-04 17:24:49 +0200

    Update lesson5.js

--- js/lesson5.js
+++ js/lesson5.js
@@ -303,37 +303,115 @@
 
 
 
-const counter = {
-    value: 0,
-    increment() {
-        console.log('increment -> this', this);
-        this.value += 1;
-    },
-    decrement() {
-        console.log('decrement -> this', this);
-        this.value -= 1;
-    },
-};
+// const counter = {
+//     value: 0,
+//     increment() {
+//         console.log('increment -> this', this);
+//         this.value += 1;
+//     },
+//     decrement() {
+//         console.log('decrement -> this', this);
+//         this.value -= 1;
+//     },
+// };
 
 
-const decrementBtn = document.querySelector('.js-decrement');
-const incrementBtn = document.querySelector('.js-increment');
-const valueEl = document.querySelector('.js-value');
+// const decrementBtn = document.querySelector('.js-decrement');
+// const incrementBtn = document.querySelector('.js-increment');
+// const valueEl = document.querySelector('.js-value');
 
-decrementBtn.addEventListener('click', function () {
-    console.log('Кликнули на декремент');
+// decrementBtn.addEventListener('click', function () {
+//     console.log('Кликнули на декремент');
 
-    counter.decrement();
-    console.log(counter);
-    valueEl.textContent = counter.value;
-});
+//     counter.decrement();
+//     console.log(counter);
+//     valueEl.textContent = counter.value;
+// });
 
-incrementBtn.addEventListener('click', function () {
-    console.log('Кликнули на инкремент');
+// incrementBtn.addEventListener('click', function () {
+//     console.log('Кликнули на инкремент');
 
-    counter.increment();
-    console.log(counter);
-    valueEl.textContent = counter.value;
-});
+//     counter.increment();
+//     console.log(counter);
+//     valueEl.textContent = counter.value;
+// });
 
-console.log(window);
+// console.log(window);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ПРОТОТИПНЕ НАСЛІДУВАННЯ
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Прототип объекта
+ *
+ * - https://miro.com/app/board/o9J_ku0WE0g=/
+ * - Object.create()
+ * - [[Prototype]] и __proto__
+ * - Object.getPrototypeOf()
+ * - Собственные свойства и Object.prototype.hasOwnProperty()
+ * - Цепочка прототипов
+ */
+
+// const objC = {
+//   z: 5,
+// };
+
+// const objB = Object.create(objC);
+// objB.y = 2;
+
+// const objA = Object.create(objB);
+// objA.x = 1;
+
+// console.log(objA.z);
+
+// console.log('objA', objA);
+
+// console.log(objA.hasOwnProperty('x'));
+
+// const dummyObj = Object.create({ message: 'Это свойство объекта протортипа' });
+// dummyObj.message = 'Это собственное свойство объекта';
+// console.log('dummyObj', dummyObj);
+
+// console.log(dummyObj.message);
+
+//  'Это собственное свойство объекта'
+//  'Это свойство на объекте-прототипе'
+
+/*
+ * Алгоритм поиска свойства в цепочке прототипов:
+ * 1. Поиск начинается в собственных свойствах
+ * 2. Если свойства нет в сообственных, поиск переходит к цепочке прототипов
+ * 3. Поиск прекращается при первом совпадении (есть такое свойство)
+ * 4. Возвращается значение свойства
+ */
+
+// const objA = Object.create({ z: 5 });
+// objA.y = 100;
+// console.log('objA', objA);
+
+// console.log(objA.x);
